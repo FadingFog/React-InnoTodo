260bd8361e02b4ea1b757a7a6199aa995b39f47c
FadingFog <aleksey.guzlov@gmail.com>
2023-06-28 18:45:50 +0300

    Removed redundant prop in TextField in RegisterPage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -7,8 +7,8 @@ export const RegisterPage = () => {
     <>
       <Typography variant="h6">Sign up for your account</Typography>
       <Stack spacing={2} component="form" autoComplete="off">
-        <TextField label="Enter email" variant="outlined" />
-        <TextField label="Enter password" variant="outlined" type="password"/>
+        <TextField label="Enter email" />
+        <TextField label="Enter password" type="password"/>
         <Button fullWidth variant="contained" sx={{textTransform: "none"}}>
           <Typography variant="h6">Continue</Typography>
         </Button>
