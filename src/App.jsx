34d9863a56c9d28cce85307f21727ec7000d5d80
FadingFog <aleksey.guzlov@gmail.com>
2023-06-28 18:38:18 +0300

    Installed @emotion/cache and removed css prefixes with CacheProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import {RouterProvider} from "react-router-dom";
 import {router} from "./routes.jsx";
 import GlobalStyles from "theme/GlobalStyles.jsx";
+import createCache from "@emotion/cache";
+import {CacheProvider} from "@emotion/react";
 
+
+const cacheNoPrefixer = createCache({
+  key: "noprefixer",
+  stylisPlugins: []
+});
 
 export default function App() {
   return (
     <>
-      <GlobalStyles />
-      <RouterProvider router={router} />
+      <CacheProvider value={cacheNoPrefixer}>
+        <GlobalStyles />
+        <RouterProvider router={router} />
+      </CacheProvider>
     </>
   )
 }
